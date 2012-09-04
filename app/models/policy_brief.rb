e5dd23b0ea0852548c03943ad9eb936c60b858b4
Jason <jason.addie@gmail.com>
2012-09-04 09:55:20 +0400

    removed unneeded policy brief score model reference

--- app/models/policy_brief.rb
+++ app/models/policy_brief.rb
@@ -3,12 +3,10 @@ class PolicyBrief < ActiveRecord::Base
   translates :analysis
   belongs_to :political_party
   belongs_to :economic_category
-  has_many :policy_brief_scores, :dependent => :destroy
   has_many :policy_brief_translations, :dependent => :destroy
   accepts_nested_attributes_for :policy_brief_translations
-  accepts_nested_attributes_for :policy_brief_scores
   attr_accessible :political_party_id, :economic_category_id,
-		:policy_brief_translations_attributes, :policy_brief_scores_attributes
+		:policy_brief_translations_attributes
 
   validates :political_party_id, :economic_category_id, :presence => true
 	validates :economic_category_id, :uniqueness => {:scope => :political_party_id,
