2fc347751c2942d36790a1f3ae54551c7a38f884
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2012-10-21 18:36:01 +0400

    slider automatization completed.

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,14 +1,37 @@
+
+function slider_auto ()
+{
+  activity.arrows[activity.direction_auto].click();
+  if (activity.index == 0)
+  {
+    activity.direction_auto = 'right';
+  }
+  else if (activity.index == (activity.slides.length - 1))
+  {
+    activity.direction_auto = 'left';
+  }
+
+  setTimeout(slider_auto, activity.delay + activity.timeout);
+}
+
 $(function ()
 {
   window.activity =
   {
     index: 0,
+    timeout: 1000,
+    delay: 3000,
+    direction_auto: 'right',
     arrows:
     {
       left : $('#activityslider .arrow[data-direction="left"]'),
       right: $('#activityslider .arrow[data-direction="right"]'),
-    }
-  }
+    },
+    slides: $('#activityslider .activities .each')
+  };
+
+  setTimeout(slider_auto, activity.delay);
+
 });
 
 
@@ -17,17 +40,26 @@ $('#activityslider .arrow:not(.disabled)').live('click', function ()
 
   var direction = $(this).data('direction');
 
-  var slides = $('#activityslider .activities .each'),
-     current = $('#activityslider .activities .each:visible');
+  var current = $('#activityslider .activities .each:visible');
 
   var next = (direction == 'left') ? -1 : 1;
   next = activity.index + next;
 
   // stop all animations
-  slides.stop(true, true);
+  activity.slides.stop(true, true);
 
-  current.fadeOut();
-  slides.eq(next).fadeIn();
+  var overflow = 150;
+
+  if (direction == 'left')
+  {
+    current.animate({top: overflow}, activity.timeout, function (){ $(this).hide(0).css('top', 0); });
+    activity.slides.eq(next).css('top', -overflow).show(0).animate({top: 0}, activity.timeout);
+  }
+  else
+  {
+    current.animate({top: -overflow}, activity.timeout, function (){ $(this).hide(0).css('top', 0); });
+    activity.slides.eq(next).css('top', overflow).show(0).animate({top: 0}, activity.timeout);
+  }
 
 
   if (next > 0 && activity.arrows.left.hasClass('disabled'))
@@ -35,7 +67,7 @@ $('#activityslider .arrow:not(.disabled)').live('click', function ()
     activity.arrows.left.removeClass('disabled');
   }
 
-  if (next < (slides.length - 1) && activity.arrows.right.hasClass('disabled'))
+  if (next < (activity.slides.length - 1) && activity.arrows.right.hasClass('disabled'))
   {
     activity.arrows.right.removeClass('disabled');
   }
@@ -44,7 +76,7 @@ $('#activityslider .arrow:not(.disabled)').live('click', function ()
   {
     activity.arrows.left.addClass('disabled');
   }
-  else if (next == (slides.length - 1))
+  else if (next == (activity.slides.length - 1))
   {
     activity.arrows.right.addClass('disabled');
   }
