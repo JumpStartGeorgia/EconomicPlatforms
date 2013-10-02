cd32f6e1d2a140792a65af0f6956efd5928b6920
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-10-02 15:09:33 +0400

    added has_many associations to election model; added language names

--- app/models/election.rb
+++ app/models/election.rb
@@ -5,6 +5,12 @@ class Election < ActiveRecord::Base
   accepts_nested_attributes_for :election_translations
   attr_accessible :date, :election_translations_attributes
 
+  has_many :activities
+  has_many :platforms
+  has_many :policy_briefs
+  has_many :political_parties
+  has_many :statements
+
   validates :date, :presence => true
 
 	default_scope order("date desc")
